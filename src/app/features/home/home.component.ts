5009933aa5bfadd2f4a7ef35dcb877a0b5d6f016
futugyou <17871902+futugyou@users.noreply.github.com>
2025-09-29 16:32:47 +0000

    Migrate homepage content to independent components

---
+++ src/app/features/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'home-root',
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class HomeComponent {
+  protected readonly title = signal('angular-project');
+}
