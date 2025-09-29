ccf9b0b38091a7aca33ea48b0784e4678d3cb97c
futugyou <17871902+futugyou@users.noreply.github.com>
2025-09-29 18:18:12 +0000

    Added order route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './features/home/home.component';
+import { OrderMainComponent } from './features/orders/order.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'orders', component: OrderMainComponent },
     { path: '**', redirectTo: '' }  
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    RouterLink,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-project');
-}
+
+}  
