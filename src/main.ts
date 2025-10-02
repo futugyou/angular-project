a17cc2d0012e9ee4c417adae0fa097a63cbacca3
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2025-10-02 12:19:06 +0000

    Set dynamic base path in main file Added global type definition to support micro-apps

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+const base = document.getElementById('angular-base')!;
+if (window.__MICRO_APP_BASE_ROUTE__) {
+  base.setAttribute('href', window.__MICRO_APP_BASE_ROUTE__);
+}
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
