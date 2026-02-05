5acc7738e659ffc2dd6946aba0724cb5159d9a4d
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2026-02-05 20:14:06 +0000

    Added a cancellable request service to handle cancellation signals.

---
+++ src/app/devui/services/cancellable-request.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, OnDestroy, signal } from '@angular/core'
+
+@Injectable()
+export class CancellableRequestService implements OnDestroy {
+  public isCancelling = signal(false)
+  private abortController: AbortController | null = null
+
+  createAbortSignal(): AbortSignal {
+    this.abortController = new AbortController()
+    this.isCancelling.set(false)
+    return this.abortController.signal
+  }
+
+  handleCancel(): void {
+    if (this.abortController) {
+      this.isCancelling.set(true)
+      this.abortController.abort()
+      this.abortController = null
+    }
+  }
+
+  resetCancelling(): void {
+    this.isCancelling.set(false)
+  }
+
+  ngOnDestroy(): void {
+    this.handleCancel()
+  }
+}
