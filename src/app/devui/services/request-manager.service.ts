8b8e3f20c363e1761dd4667a4a804ef9cb03fb05
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2026-02-05 20:18:06 +0000

    Updated request management service, improving cancellation signal handling and state management.

--- src/app/devui/services/request-manager.service.ts
+++ src/app/devui/services/request-manager.service.ts
@@ -1,23 +1,40 @@
 import { Injectable, OnDestroy } from '@angular/core'
-import { Subject, takeUntil } from 'rxjs'
+import { HttpClient } from '@angular/common/http'
+import { BehaviorSubject, Observable, Subject, of } from 'rxjs'
+import { catchError, takeUntil, finalize, tap } from 'rxjs/operators'
 
 @Injectable()
 export class RequestManagerService implements OnDestroy {
-  private cancel$ = new Subject<void>()
-  public isCancelling = false
+  private isCancellingSubject = new BehaviorSubject<boolean>(false)
+  public isCancelling$ = this.isCancellingSubject.asObservable()
 
-  get cancelSignal() {
-    return this.cancel$.asObservable()
+  private cancelSignal$ = new Subject<void>()
+
+  constructor(private http: HttpClient) {}
+
+  executeRequest(url: string): Observable<any> {
+    this.isCancellingSubject.next(false)
+
+    return this.http.get(url).pipe(
+      takeUntil(this.cancelSignal$),
+      tap(() => console.log('Request completed successfully.')),
+      catchError((err) => {
+        throw err
+      }),
+      finalize(() => {
+        this.isCancellingSubject.next(false)
+      }),
+    )
   }
 
-  cancel() {
-    this.isCancelling = true
-    this.cancel$.next()
-    this.isCancelling = false
+  cancel(): void {
+    this.isCancellingSubject.next(true)
+    this.cancelSignal$.next()
+    setTimeout(() => this.isCancellingSubject.next(false), 500)
   }
 
-  ngOnDestroy() {
-    this.cancel$.next()
-    this.cancel$.complete()
+  ngOnDestroy(): void {
+    this.cancelSignal$.next()
+    this.cancelSignal$.complete()
   }
 }
