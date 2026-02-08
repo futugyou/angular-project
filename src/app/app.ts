6e48e7855aa1cb1279899378d6a7eb00b8dc755d
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2026-02-08 20:16:11 +0000

    Optimize component import and template syntax

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core'
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router'
-import { CommonModule } from '@angular/common'
+
 import { NgIconsModule } from '@ng-icons/core'
 import { CdkMenuModule } from '@angular/cdk/menu'
 
 @Component({
   selector: 'angular-app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, CdkMenuModule, NgIconsModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CdkMenuModule, NgIconsModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/app/devui/components/ui/attachment-preview.component.ts
+++ src/app/devui/components/ui/attachment-preview.component.ts
@@ -1,73 +1,73 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core'
-import { CommonModule } from '@angular/common'
+
 import { AttachmentItem } from './types'
 import { NgIconsModule } from '@ng-icons/core'
 
 @Component({
   selector: 'app-attachment-preview',
   standalone: true,
-  imports: [CommonModule, NgIconsModule],
+  imports: [NgIconsModule],
   template: `
     <div
       class="relative w-16 h-16 rounded border overflow-hidden group cursor-pointer"
       [title]="attachment.file.name"
-    >
-      <ng-container [ngSwitch]="attachment.type">
-        <ng-container *ngSwitchCase="'image'">
-          <img
-            *ngIf="attachment.preview; else noImage"
-            [src]="attachment.preview"
-            [alt]="attachment.file.name"
-            class="w-full h-full object-cover"
-          />
-          <ng-template #noImage>
+      >
+      @switch (attachment.type) {
+        @case ('image') {
+          @if (attachment.preview) {
+            <img
+              [src]="attachment.preview"
+              [alt]="attachment.file.name"
+              class="w-full h-full object-cover"
+              />
+          } @else {
             <div class="flex items-center justify-center w-full h-full bg-gray-200">
               <ng-icon name="lucideImage" class="h-6 w-6 text-gray-400"></ng-icon>
             </div>
-          </ng-template>
-        </ng-container>
-
-        <div
-          *ngSwitchCase="'pdf'"
-          class="flex flex-col items-center justify-center w-full h-full bg-red-50"
-        >
-          <ng-icon name="lucideFileText" class="h-6 w-6 text-red-500 mb-1"></ng-icon>
-          <span class="text-xs text-red-600">PDF</span>
-        </div>
-
-        <div
-          *ngSwitchCase="'audio'"
-          class="flex flex-col items-center justify-center w-full h-full bg-purple-50"
-        >
-          <ng-icon name="lucideMusic" class="h-6 w-6 text-purple-500 mb-1"></ng-icon>
-          <span class="text-xs text-purple-600">AUDIO</span>
-        </div>
-
-        <div
-          *ngSwitchDefault
-          class="flex flex-col items-center justify-center w-full h-full bg-gray-100"
-        >
-          <ng-icon name="lucideFileText" class="h-6 w-6 text-gray-500 mb-1"></ng-icon>
-          <span class="text-xs text-gray-600">FILE</span>
-        </div>
-      </ng-container>
-
+          }
+        }
+        @case ('pdf') {
+          <div
+            class="flex flex-col items-center justify-center w-full h-full bg-red-50"
+            >
+            <ng-icon name="lucideFileText" class="h-6 w-6 text-red-500 mb-1"></ng-icon>
+            <span class="text-xs text-red-600">PDF</span>
+          </div>
+        }
+        @case ('audio') {
+          <div
+            class="flex flex-col items-center justify-center w-full h-full bg-purple-50"
+            >
+            <ng-icon name="lucideMusic" class="h-6 w-6 text-purple-500 mb-1"></ng-icon>
+            <span class="text-xs text-purple-600">AUDIO</span>
+          </div>
+        }
+        @default {
+          <div
+            class="flex flex-col items-center justify-center w-full h-full bg-gray-100"
+            >
+            <ng-icon name="lucideFileText" class="h-6 w-6 text-gray-500 mb-1"></ng-icon>
+            <span class="text-xs text-gray-600">FILE</span>
+          </div>
+        }
+      }
+    
       <div
         (click)="remove.emit()"
         class="absolute inset-0 bg-black/60 flex items-center justify-center transition-all duration-200 ease-in-out opacity-0 group-hover:opacity-100 backdrop-blur-sm"
-      >
+        >
         <div class="scale-75 group-hover:scale-100 transition-all duration-200">
           <ng-icon name="lucideTrash2" class="h-5 w-5 text-white drop-shadow-lg"></ng-icon>
         </div>
       </div>
-
+    
       <div
         class="absolute bottom-0 left-0 right-0 bg-black bg-opacity-75 text-white text-xs p-1 truncate opacity-0 group-hover:opacity-100 transition-opacity duration-200"
-      >
+        >
         {{ attachment.file.name }}
       </div>
     </div>
-  `,
+    `,
 })
 export class AttachmentPreviewComponent {
   @Input({ required: true }) attachment!: AttachmentItem
