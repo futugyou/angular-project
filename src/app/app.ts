eaa97e1b5ad2867ef6789459bf33049c6de39a7a
futugyou <17871902+futugyou@users.noreply.github.com>
2026-04-04 22:57:51 +0800

    Added fullscreen-toggle component to app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,17 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router'
 
 import { NgIconsModule } from '@ng-icons/core'
 import { MenuStack, MENU_STACK } from '@angular/cdk/menu'
+import { LayoutService } from './devui/services/layout.service'
+import { FullscreenToggleComponent } from './devui/components/layout/fullscreen-toggle.component'
 
 @Component({
   selector: 'angular-app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgIconsModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgIconsModule, FullscreenToggleComponent],
   providers: [{ provide: MENU_STACK, useClass: MenuStack }],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App {
+  constructor(public layoutService: LayoutService) {}
+}
