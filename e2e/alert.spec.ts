b8773a4bb38ed5708bc2dcf5cfbec3ec4887e7b9
futugyou <17871902+futugyou@users.noreply.github.com>
2026-04-06 23:13:31 +0800

    Added e2e test

---
+++ e2e/alert.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Alert E2E', () => {
+  test('The accessibility role should be displayed correctly on the page.', async ({ page }) => {
+    await page.goto('/testing')
+
+    const alert = page.locator('app-alert')
+    await expect(alert).toBeVisible()
+    await expect(alert).toHaveAttribute('role', 'alert')
+
+    const title = alert.locator('app-alert-title')
+    await expect(title).toContainText('Update Available')
+  })
+})
