a5441abd6524dff5e0f7beaf1a8ae59fa57f3985
futugyou <17871902+futugyou@users.noreply.github.com>
2026-04-11 00:26:32 +0800

    Added dialog storybook

---
+++ src/app/shared/ui/dialog/dialog.stories.ts
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from '@storybook/angular'
+import { moduleMetadata, componentWrapperDecorator } from '@storybook/angular'
+import { DIALOG_COMPONENTS } from './index'
+import { signal } from '@angular/core'
+import { NgIconComponent, provideIcons } from '@ng-icons/core'
+import { lucideX } from '@ng-icons/lucide'
+
+const meta: Meta = {
+  title: 'Components/Dialog',
+  decorators: [
+    moduleMetadata({
+      imports: [...DIALOG_COMPONENTS, NgIconComponent],
+      providers: [provideIcons({ lucideX })],
+    }),
+  ],
+}
+
+export default meta
+
+export const Basic: StoryObj = {
+  render: () => ({
+    props: {
+      isOpen: signal(true),
+    },
+    template: `
+      <app-dialog [open]="isOpen()">
+        <app-dialog-header>
+          <app-dialog-title>Modal Title</app-dialog-title>
+          <app-dialog-description>This is a description for the dialog.</app-dialog-description>
+        </app-dialog-header>
+        <app-dialog-content>
+          <p>Main content area with scrollable behavior enabled by default.</p>
+        </app-dialog-content>
+        <app-dialog-footer>
+          <button (click)="isOpen.set(false)">Cancel</button>
+        </app-dialog-footer>
+      </app-dialog>
+    `,
+  }),
+}
+
+export const CustomSize: StoryObj = {
+  render: () => ({
+    props: {
+      isOpen: signal(true),
+    },
+    template: `
+      <app-dialog [open]="isOpen()" class="max-w-sm min-h-[200px]">
+        <app-dialog-header>
+          <app-dialog-title>Small Dialog</app-dialog-title>
+        </app-dialog-header>
+        <app-dialog-content>
+          This dialog uses a custom Tailwind class for sizing.
+        </app-dialog-content>
+      </app-dialog>
+    `,
+  }),
+}
